c3a5ecbbf62e743a271d96e7ed110c9b384e0be1
mjazy <112projects.contact@gmail.com>
2018-11-03 17:35:02 +0100

    4th commit: generated Server project added

---
+++ Server/src/main/java/com/Project3/Server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.Project3.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
