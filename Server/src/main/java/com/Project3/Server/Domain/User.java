d7c3d0feff88156ed9fabd844f0bf4944a4dd0f3
mjazy <112projects.contact@gmail.com>
2018-11-25 03:26:05 +0100

    Commit 14th: additional User constructor added

--- Server/src/main/java/com/Project3/Server/Domain/User.java
+++ Server/src/main/java/com/Project3/Server/Domain/User.java
@@ -37,6 +37,15 @@ public class User{
 	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
 
+	public User(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public User() {
+	}
+
 	public int getId() {
 		return id;
 	}
