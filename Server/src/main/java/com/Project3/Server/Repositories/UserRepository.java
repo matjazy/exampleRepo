eb2db26a196c23b64fdd59127feca66594f6e4d3
mjazy <112projects.contact@gmail.com>
2019-03-23 18:52:09 +0100

    Commit 22nd: deleteByUsername method added to UserRepository

--- Server/src/main/java/com/Project3/Server/Repositories/UserRepository.java
+++ Server/src/main/java/com/Project3/Server/Repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.Project3.Server.Repositories;
 import java.util.List;
 import java.util.Set;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,21 +13,24 @@ import com.Project3.Server.Domain.User;
 
 /**
  * Repository related to User model.
+ * 
  * @author MJazy
  *
  */
 @Repository("userRepository")
-public interface UserRepository extends CrudRepository <User, Integer>{
-	
+public interface UserRepository extends CrudRepository<User, Integer> {
+
 	/**
 	 * Allows to find particular user by username.
+	 * 
 	 * @param username of particular user.
 	 * @return user with relevant username or null.
 	 */
 	User findByUsername(String username);
-	
+
 	/**
 	 * Allows to find users by email.
+	 * 
 	 * @param email of a user.
 	 * @return List<User> with relevant email.
 	 */
@@ -33,9 +38,18 @@ public interface UserRepository extends CrudRepository <User, Integer>{
 
 	/**
 	 * Allows to find users by roles.
+	 * 
 	 * @param roles of a user.
 	 * @return List<User> with relevant roles.
 	 */
 	List<User> findByRolesIn(Set<Role> roles);
-	
+
+	/**
+	 * Allows to delete user by username.
+	 * 
+	 * @param username of user to be deleted.
+	 */
+	@Transactional
+	public Integer deleteByUsername(String username);
+
 }
--- Server/src/test/java/com/Project3/Server/Repositories/UserRepositoryTest.java
+++ Server/src/test/java/com/Project3/Server/Repositories/UserRepositoryTest.java
@@ -1,9 +1,9 @@
 package com.Project3.Server.Repositories;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertEquals;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -24,54 +24,63 @@ public class UserRepositoryTest {
 
 	@Inject
 	UserRepository userRepository;
-	
-	User user, user1;
-	
-	
+
+	User user, user1, user2;
+
 	@Before
 	public void setupDatabase() {
-		
+
 		Set<Role> testRoles = new HashSet<Role>();
-		
+
 		user = new User();
-		String userTestString = "test";
-		user.setUsername(userTestString);
-		user.setEmail(userTestString);
-		user.setPassword(userTestString);
-		user.setRoles(testRoles);
-		userRepository.save(user);
-		
 		user1 = new User();
-		String user1TestString = "test1";
-		user1.setUsername(user1TestString);
-		user1.setEmail(user1TestString);
-		user1.setPassword(user1TestString);
-		user1.setRoles(testRoles);
-		userRepository.save(user1);
+		user2 = new User();
+
+		List<User> users = List.of(user, user1, user2);
+
+		int index = 0;
+		for (User user : users) {
+			String userTestString = "test" + index;
+			user.setUsername(userTestString);
+			user.setEmail(userTestString);
+			user.setPassword(userTestString);
+			user.setRoles(testRoles);
+			userRepository.save(user);
+			index++;
+		}
 	}
-	
+
 	@Test
 	public void findByUsernameTest() {
 		User storedUser = userRepository.findByUsername(user.getUsername());
 		User storedUser1 = userRepository.findByUsername(user1.getUsername());
-		
+
 		assertEquals(user.getId(), storedUser.getId());
 		assertEquals(user.getUsername(), storedUser.getUsername());
 		assertEquals(user.getPassword(), storedUser.getPassword());
 		assertEquals(user.getEmail(), storedUser.getEmail());
 		assertEquals(user.getRoles(), storedUser.getRoles());
-		
+
 		assertEquals(user1.getId(), storedUser1.getId());
 		assertEquals(user1.getUsername(), storedUser1.getUsername());
 		assertEquals(user1.getPassword(), storedUser1.getPassword());
 		assertEquals(user1.getEmail(), storedUser1.getEmail());
 		assertEquals(user1.getRoles(), storedUser1.getRoles());
 	}
-	
+
+	@Test
+	public void testDeleteByUsername() {
+
+		userRepository.deleteByUsername(user2.getUsername());
+		User deletedUser = userRepository.findByUsername(user2.getUsername());
+		assertEquals(deletedUser, null);
+	}
+
 	@After
 	public void clearDatabase() {
 		userRepository.delete(user);
 		userRepository.delete(user1);
+		userRepository.delete(user2);
 	}
-	
+
 }
